68c36b56188b9dd86fe9adb845aa95df5484b5bf
Tom Besio <thomas.besio@gmail.com>
2015-04-21 11:19:53 -0500

    Updated readme and some additions to main file

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 #Homework 3
+
+[1] Interpret AICc selected model from my nhlreg lasso.
+Just tell some stories about what the model tells you.
+
+[2] The gamlr run for nhlreg uses standardize=FALSE.
+Why did I do this? What happens if you do standardize?
+
+[3] Compare model selection methods for the nhlreg lasso.
+Consider both IC and CV (you’ll want to create cv.nhlreg).
+
+[4] We’ve controlled our estimates for confounding information
+from team effects and special play configuration. How do things
+change if we ignored this info (i.e., fit a player-only model)?
+Which scheme is better (interpretability, CV, and IC)?
+
+[+] Can you translate player Bk effects into something
+comparable to classic Plus-Minus? How do things compare?
--- hockey_start.R
+++ hockey_start.R
@@ -1,7 +1,7 @@
 ## nhl hockey analysis
 
-## the data is in gamlr.  
-## You need to first install this, 
+## the data is in gamlr.
+## You need to first install this,
 ## via install.packages("gamlr")
 
 library(gamlr) # loads Matrix as well
@@ -15,12 +15,21 @@ x <- cBind(config,team,player) # cBind binds together two sparse matrices
 # build 'y': home vs away, binary response
 y <- goal$homegoal
 
-nhlreg <- gamlr(x, y, 
+nhlreg <- gamlr(x, y,
 	free=1:(ncol(config)+ncol(team)), ## free denotes unpenalized columns
 	family="binomial", standardize=FALSE)
 
 ## coefficients (grab only the players)
-# AICc selection 
+# AICc selection
 Baicc <- coef(nhlreg)[colnames(player),]
 
+# BIC selection
+# We see that under BIC, the players have no effect only the configuration and the team season
+Bbic <- coef(nhlreg, select=which.min(BIC(nhlreg)))
+
+# Q3 add in the cross validation gammo lasso
+
+cv.nhlreg <- cv.gamlr(x, y,
+  free=1:(ncol(config)+ncol(team)), ## free denotes unpenalized columns
+  family="binomial", standardize=FALSE)
 
